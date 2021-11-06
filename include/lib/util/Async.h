56e8f3f576a0f7c38f5228273c650c251f89a2bb
Marco Tan <marco.tan.200405@gmail.com>
2021-11-06 19:06:03 -0400

    async stuff

---
+++ include/lib/util/Async.h
@@ -0,0 +1,52 @@
+//* bad library
+//* utility
+//* Async - Asynchronous execution
+
+//* header guard
+#pragma once
+
+//* pros api
+#include "api.h"
+
+//* other headers
+#include "lib/misc/Units.h"
+
+//* stl
+// #include <atomic>
+#include <functional>
+#include <queue>
+#include <utility>
+
+namespace Lib1104A {
+    namespace Util {
+        using async_func_t = std::pair<std::function<void()>, Misc::ms_t>;
+
+        class Async {
+            public:
+                //? ctor & dtor
+                Async();
+                ~Async();
+
+                //? setters
+
+                // add to queue
+                Async& add(std::function<void()> func, Misc::ms_t delay);
+
+                //? getters
+
+                //? methods
+            
+            private:
+                //? members
+
+                // queue
+                std::queue<async_func_t> m_queue;
+                // thread
+                pros::Task m_task;
+
+                //? private methods
+                // run the queue
+                void run_queue();
+        };
+    }
+}
---
+++ src/lib/util/Async.cpp
@@ -0,0 +1,49 @@
+//* bad library
+//* utility
+//* Async - Asynchronous execution
+
+//* headers
+#include "lib/util/Async.h"
+
+namespace Lib1104A {
+    namespace Util 
+    {
+        Async::Async()
+            : m_queue {},
+              m_task {std::bind(&Async::run_queue, this)}
+        {
+            //* do nothing
+        }
+
+        Async::~Async()
+        {
+            m_task.remove();
+            std::queue<async_func_t>().swap(m_queue);
+        }
+
+        Async& Async::add(std::function<void()> func, Misc::ms_t delay)
+        {
+            m_queue.push(std::make_pair(func, delay));
+            m_task.notify();
+            return *this;
+        }
+
+        void Async::run_queue()
+        {
+            while (!pros::competition::is_disabled() && m_task.notify_take(0, TIMEOUT_MAX))
+            {
+                while (!m_queue.empty())
+                {
+                    auto& func = m_queue.front().first;
+                    auto& delay = m_queue.front().second;
+                    if (delay > 0)
+                    {
+                        pros::Task::delay(delay);
+                    }
+                    func();
+                    m_queue.pop();
+                }
+            }
+        }
+    }
+}
