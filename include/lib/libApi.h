813b2e7f3e3476a4d6f58b512dfa2a8e96c59a1d
Marco Tan <marco.tan.200405@gmail.com>
2021-11-27 12:32:05 -0500

    api header

---
+++ include/lib/libApi.h
@@ -0,0 +1,18 @@
+//* bad library
+//* api header
+
+//* header guard
+#pragma once
+
+//* control
+#include "lib/control/filter/Filter.h"
+#include "lib/control/PID.h"
+#include "lib/control/TrapezoidalMotion.h"
+
+//* misc
+#include "lib/misc/RobotConst.h"
+#include "lib/misc/Units.h"
+
+//* util
+#include "lib/util/Async.h"
+#include "lib/util/Timer.h"
